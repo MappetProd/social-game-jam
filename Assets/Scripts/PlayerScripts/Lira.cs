1388cf99547c42718f73be045f4979f82fa97a23
unlimpain <watniger@gmail.com>
2024-02-11 17:40:56 +0300

    add reload to lira ability

--- Assets/Scripts/PlayerScripts/Lira.cs
+++ Assets/Scripts/PlayerScripts/Lira.cs
@@ -7,17 +7,29 @@ namespace PlayerScripts
     public class Lira : MonoBehaviour
     {
         [SerializeField] private Light2D _enemyLight;
-    
+        [SerializeField] private float _lightIntensity;
+        [SerializeField] private float _enemyLightCooldown;
+        private bool _isAbilityReady = true;
+        
         public void Play()
         {
+            if (!_isAbilityReady) return;
+            
+            Invoke(nameof(ReloadAbility), _enemyLightCooldown);
             StartCoroutine(EnemyLight());
+            _isAbilityReady = false;
         }
 
         private IEnumerator EnemyLight()
         {
-            _enemyLight.intensity = 1f;
+            _enemyLight.intensity = _lightIntensity;
             yield return new WaitForSeconds(3f);
             _enemyLight.intensity = 0f;
         }
+
+        private void ReloadAbility()
+        {
+            _isAbilityReady = true;
+        }
     }
-}
+} 
