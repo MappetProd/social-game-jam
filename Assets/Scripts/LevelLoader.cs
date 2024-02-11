89fcb8d2bc9a71cadb40831f8969d70cbe8de672
MappetProd <fwexample821@gmail.com>
2024-02-11 15:21:33 +0300

    cutscenes added (with bug)

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static UnityEngine.Rendering.DebugUI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator animator;
+    public float transitionTime = 1f;
+
+    [SerializeField]
+    private bool isCutscene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isCutscene)
+        {
+            LoadNextLevel();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNextLevel()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        StartCoroutine(LoadLevel(nextSceneIndex));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        yield return new WaitForSeconds(transitionTime);
+        animator.SetTrigger("Start");
+        yield return new WaitForSeconds(1f);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
